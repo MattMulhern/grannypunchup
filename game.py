8e0e4e5257b23893d0256c2154138069804f00ef
Michael Robinson <mrobinson@proofpoint.com>
2019-01-26 11:41:41 +0000

    Add enemies

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import logging
 import sys
 import pyxel
 import settings
-from sprites import Player
+from sprites import Player, Enemy
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -19,12 +19,18 @@ class Game:
                         Player("Betrice", 50, 50, velocity=(0, 0), player_num=2),
                         Player("Candice", 50, 50, velocity=(0, 0), player_num=3),
                         Player("Derp", 50, 50, velocity=(0, 0), player_num=4)]
+        self.enemies = [Enemy("Adolf", 100, 50, velocity=(0, 0)),
+                        Enemy("Jean Claude Grand Ma", 100, 50, velocity=(0, 0)),
+                        Enemy("Cunt ripper", 100, 50, velocity=(0, 0)),
+                        Enemy("Dick cheese", 100, 50, velocity=(0, 0))]
         pyxel.tilemap(0).set(
             0, 0, ["0202020401006061620040", "4203202122030001020360", "0202020401006061620040"], 0
         )
 
         for player in self.players:
             self.phys.add(player.body, player.poly)
+        for enemy in self.enemies:
+            self.phys.add(enemy.body, enemy.poly)
 
     def _init_space(self):
         """ gravity, canvas etc """
@@ -34,6 +40,8 @@ class Game:
         """ update game objects """
         for player in self.players:
             player.update()
+        for enemy in self.enemies:
+            enemy.update()
         self.phys.step(settings.phys_dt)
 
     def draw(self):
@@ -41,4 +49,6 @@ class Game:
         pyxel.text(10, 5, "Granny Punch Up", 14)
         for player in self.players:
             player.draw()
+        for enemy in self.enemies:
+            enemy.draw()
         pyxel.bltm(0, 0, 0, 0, 0, 100, 100, 0)
--- sprites.py
+++ sprites.py
@@ -2,25 +2,32 @@ import logging
 import sys
 import pymunk
 import pyxel
-from sprite import Sprite
-from pymunk import Vec2d
-
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-class Player(Sprite):
-    """ test class """
-    def __init__(self, id, xpos, ypos, imagebank=0, spritesheet_xpos=48, spritesheet_ypos=0, width=16, height=16,
-                 spritesheet_keycol=0, mass=1, momentum=1, velocity=(0, 0), player_num=1):
-        spritesheet_ypos = spritesheet_ypos + ((player_num - 1) * height)
-        print(spritesheet_xpos, spritesheet_ypos)
-        super().__init__(id, xpos, ypos, imagebank, spritesheet_xpos, spritesheet_ypos, width, height,
-                         spritesheet_keycol, mass, momentum, velocity)
+class Sprite:
+    """ Base Sprite clas. """
+    def __init__(self,
+                 id, xpos, ypos,
+                 imagebank, spritesheet_xpos, spritesheet_ypos, width, height, spritesheet_keycol,
+                 mass, momentum, velocity):
+        self.id = id
+        self.xpos0 = xpos
+        self.ypos0 = ypos
+        self.imagebank = imagebank
+        self.spritesheet_xpos = spritesheet_xpos
+        self.spritesheet_ypos = spritesheet_ypos
+        self.width = width
+        self.height = height
+        self.spritesheet_keycol = spritesheet_keycol
+        self.mass = mass
+        self.momentum = momentum
 
-        self.poly = pymunk.Circle(self.body, (self.width / 2), offset=(0, 0))
-        self.player_num = player_num
+        self.body = pymunk.Body(self.mass, self.momentum)
+        self.body.position = self.xpos0, self.ypos0
+        self.body.velocity = velocity
 
     def draw(self):
         if self.body.velocity != (0, 0):
@@ -35,8 +42,23 @@ class Player(Sprite):
                   self.height,
                   self.spritesheet_keycol)
 
+
+class Player(Sprite):
+    """ Gamepad player class """
+    def __init__(self, id, xpos, ypos, imagebank=0, spritesheet_xpos=48, spritesheet_ypos=0, width=16, height=16,
+                 spritesheet_keycol=0, mass=1, momentum=1, velocity=(0, 0), player_num=1):
+        spritesheet_ypos = spritesheet_ypos + ((player_num - 1) * height)
+        print(spritesheet_xpos, spritesheet_ypos)
+        super().__init__(id, xpos, ypos, imagebank, spritesheet_xpos, spritesheet_ypos, width, height,
+                         spritesheet_keycol, mass, momentum, velocity)
+
+        self.poly = pymunk.Circle(self.body, (self.width / 2), offset=(0, 0))
+        self.player_num = player_num
+
     def update(self):
         num = self.player_num
+        print(f"GAMEPAD_{num}_UP")
+        print(getattr(pyxel, f"GAMEPAD_{num}_UP"))
         if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(getattr(pyxel, f"GAMEPAD_{num}_UP")):
             self.body.apply_impulse_at_local_point((0, -10), (0, 0))
         if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(getattr(pyxel, f"GAMEPAD_{num}_DOWN")):
@@ -48,3 +70,18 @@ class Player(Sprite):
         if pyxel.btnr(getattr(pyxel, f"GAMEPAD_{num}_UP")) and pyxel.btnr(getattr(pyxel, f"GAMEPAD_{num}_DOWN")) and pyxel.btnr(getattr(pyxel, f"GAMEPAD_{num}_RIGHT")) and pyxel.btnr(getattr(pyxel, f"GAMEPAD_{num}_LEFT")):
             print('Released')
             # self.body.velocity(0, 0)
+
+class Enemy(Sprite):
+    """ Gamepad player class """
+    def __init__(self, id, xpos, ypos, imagebank=0, spritesheet_xpos=64, spritesheet_ypos=64, width=16, height=16,
+                 spritesheet_keycol=0, mass=1, momentum=1, velocity=(0, 0), player_num=1):
+        spritesheet_ypos = spritesheet_ypos + ((player_num - 1) * height)
+        print(spritesheet_xpos, spritesheet_ypos)
+        super().__init__(id, xpos, ypos, imagebank, spritesheet_xpos, spritesheet_ypos, width, height,
+                         spritesheet_keycol, mass, momentum, velocity)
+
+        self.poly = pymunk.Circle(self.body, (self.width / 2), offset=(0, 0))
+        self.player_num = player_num
+
+    def update(self):
+            print('Enemy update')
