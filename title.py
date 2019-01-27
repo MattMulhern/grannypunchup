3a2d889d0ab9f65abaab5371c559b00b313230b3
Michael Robinson <mrobinson@proofpoint.com>
2019-01-27 17:15:29 +0000

    Names

--- title.py
+++ title.py
@@ -19,3 +19,7 @@ class Title:
     def draw(self):
         """ draw title to canvas """
         pyxel.blt(0, 0, 2, 0, 0, 255, 128, 0)
+        pyxel.text(9, 2, "Jean claude Grand Ma", 12)
+        pyxel.text(170, 2, "Grandma-ster Flash", 8)
+        pyxel.text(190, 135, "Dolph LunGran", 11)
+        pyxel.text(9, 135, "Jackie Gran", 7)
